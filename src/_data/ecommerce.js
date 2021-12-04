2b7194e949a193c5d369a923ff8c051e1785547d
Ted Martin <etmartinkazoo@gmail.com>
2021-12-04 16:05:05 -0500

    Add locals

---
+++ src/_data/ecommerce.js
@@ -0,0 +1,7 @@
+const fetch = require("node-fetch");
+
+module.exports = async function() {
+  let url = "https://api.standupmichigan.com/items/ecommerce?access_token=WvqjmA5UwADGLZ"
+  return await fetch(url)
+    .then(data => data.json())
+};
