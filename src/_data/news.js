3c485fe1216f3037553643d2e6066eb67e797b80
Ted Martin <etmartinkazoo@gmail.com>
2021-12-05 09:11:05 -0500

    Home hero

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 
 module.exports = async function() {
-  let url = "https://api.standupmichigan.com/items/news?sort=-published_on&access_token=WvqjmA5UwADGLZ"
+  let url = "https://api.standupmichigan.com/items/news?access_token=WvqjmA5UwADGLZ"
   return await fetch(url)
     .then(data => data.json())
 };
