cc8a3485ab177ad3afc876fd150c84998f3211ea
Ted Martin <etmartinkazoo@gmail.com>
2021-12-20 18:28:56 -0500

    Assets icons

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
     let pathPrefix = "/";
     eleventyConfig.addPassthroughCopy("src/assets/css");
     eleventyConfig.addPassthroughCopy("src/assets/fonts");
+    eleventyConfig.addPassthroughCopy("src/assets/icons");
     eleventyConfig.addPassthroughCopy("src/assets/images");
     eleventyConfig.addPassthroughCopy("src/assets/js");
     return {
